988f09ceff82bcae070a3e66cb7ce638a29f6873
etale <taro.terao@gmail.com>
2016-02-04 17:22:40 +0900

    test service worker

---
+++ serviceWorker.js
@@ -0,0 +1,10 @@
+self.addEventListener('fetch', event =>
+  event.respondWith(
+    caches.open('v1')
+    .then(cache =>
+      cache.match(event.request)
+      .then(response =>
+        response ||
+        fetch(event.request.clone()).then(response =>
+          cache.put(event.request, response.clone())
+        )))))
