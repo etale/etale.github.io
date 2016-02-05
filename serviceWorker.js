ecf174f4876928012d40655b216fd3688b2f423d
etale <taro.terao@gmail.com>
2016-02-05 12:10:10 +0900

    modify slightly

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,11 @@
-self.addEventListener('fetch', event =>
+addEventListener('fetch', event =>
   event.respondWith(
     caches.open('v1')
     .then(cache =>
       cache.match(event.request)
       .then(response =>
         response ||
-        fetch(event.request.clone()).then(response =>
-          (cache.put(event.request, response.clone(), response)))))))
+        fetch(event.request.clone())
+        .then(response =>
+          cache.put(event.request, response.clone())
+        .then(() => response))))))
