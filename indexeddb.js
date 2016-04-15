3fd0c6b2d3f3e6e8c34c24b6f9c4cba158fc939b
etale <taro.terao@gmail.com>
2016-04-15 17:28:48 +0900

    es2015 style

--- indexeddb.js
+++ indexeddb.js
@@ -1,48 +1,39 @@
-var
-log = console.log.bind(console),
-id = function (a) {
-  var _ = new Uint32Array(a)
-  crypto.getRandomValues(_)
-  return Array.prototype.map.call(_, function (a) {
-    a = a.toString(16)
-    return ['00000000'.slice(a.length), a].join('')
-  }).join('')
-},
-db,
-req = indexedDB.open('db'),
-put = function (k, v) {
-  return new Promise(function (resolve, reject) {
-    var req = db.transaction('store', 'readwrite').objectStore('store').put(v, k)
+var log = console.log.bind(console)
+, id = (a) =>
+Array.from(crypto.getRandomValues(new Uint8Array(a)))
+.map(a => a + 0x100).map(a => a.toString(0x10).slice(1))
+.join('')
+, db
+, req = indexedDB.open('db')
+, put = (k, v) =>
+new Promise((f, r) => {
+  var req = db.transaction('store', 'readwrite').objectStore('store').put(v, k)
+  req.onsuccess = () => f(req.result)
+  req.onerror = () => r(req.error)
+})
+, get = (k) =>
+new Promise((f, r) => {
+  var req = db.transaction('store').objectStore('store').get(k)
+  req.onsuccess = () => f(req.result)
+  req.onerror = () => r(req.error)
+})
+, del = (k) =>
+new Promise((f, r) => {
+  var req = db.transaction('store', 'readwrite').objectStore('store').delete(k)
+  req.onsuccess = () => f(req.result)
+  req.onerror = () => r(req.error)
+})
 
-    req.onsuccess = function () { resolve(this.result) }
-    req.onerror = function () { reject(this.error) }
-  })
-},
-get = function (k) {
-  return new Promise(function (resolve, reject) {
-    var req = db.transaction('store').objectStore('store').get(k)
-
-    req.onsuccess = function () { resolve(this.result) }
-    req.onerror = function () { reject(this.error) }
-  })
-},
-del = function (k) {
-  return new Promise(function (resolve, reject) {
-    var req = db.transaction('store', 'readwrite').objectStore('store').delete(k)
-
-    req.onsuccess = function () { resolve(this.result) }
-    req.onerror = function () { reject(this.error) }
-  })
-}
-
-req.onupgradeneeded = function () {
-  db = this.result
+req.onupgradeneeded = () => {
+  db = req.result
   db.createObjectStore('store')
   console.info('ObjectStore \'store\' is created.')
 }
-req.onsuccess = function () {
-  db = this.result
+req.onsuccess = () => {
+  db = req.result
+  console.info('success: indexedDB.open(\'db\')')
 }
-req.onerror = function () {
-  console.error(this.error)
+req.onerror = () => {
+  console.error('error: indexedDB.open(\'db\')')
+  console.error(req.error)
 }
