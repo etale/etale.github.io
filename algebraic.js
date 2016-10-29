9c705fe2c4ee5d58cb7219c303014a2838bc5bac
etale <taro.terao@gmail.com>
2016-10-29 09:46:32 +0900

    u

--- algebraic.js
+++ algebraic.js
@@ -1,7 +1,8 @@
 class Algebraic {
   eq(a) {
     return (
-      Reflect.getPrototypeOf(this) === Reflect.getPrototypeOf(Object(a))
+      Reflect.getPrototypeOf(this) ===
+      Reflect.getPrototypeOf(Object(a))
     )
   }
   eql(a) {
@@ -29,10 +30,12 @@ class Algebraic {
   }
   get _neg() { return new Error('_neg is missing') }
   add(a) {
-    return (([_, a]) => (
-      _._add(a).finalize
-    ))
-    (this.coerce(a))
+    return (
+      (([_, a]) => (
+        _._add(a).finalize
+      ))
+      (this.coerce(a))
+    )
   }
   _add(a) { return new Error('_add is missing') }
   get unity() {
@@ -71,7 +74,7 @@ class Algebraic {
 }
 let toUint8Array = (a) => (
   a < 0 ? (
-    new Integer(toUint8Array(-a))._
+    new Integer(toUint8Array(- a)).neg._
   ) : (
     ((arr) => (
       new Uint8Array(arr.last < 0x80 ? arr : [...arr, 0])
@@ -87,7 +90,7 @@ Object.defineProperties(Array.prototype, {
     }
   },
   _join: {
-    value(a = 0x100) {
+    value(a) {
       return (
         this.reverse().reduce((_, e) => (
           _ * a + e
@@ -334,9 +337,6 @@ Object.defineProperties(Number.prototype, {
     }
   },
 
-
-
-
   exp: {
     get() {
       return Math.exp(this)
@@ -393,14 +393,14 @@ class Integer extends Algebraic {
 
     return (
       a.length < 6
-      ? a._join()
+      ? a._join(0x100)
       : new Integer(new Uint8Array(a))
     )
   }
   get float() {
     return (
       ((_) => (
-        Array.from(_)._join()
+        Array.from(_)._join(0x100)
       ))
       (this._)
     )
@@ -485,39 +485,38 @@ class Integer extends Algebraic {
       )
     )
   }
-  get _unity() { return Integer.unity }
+  get _unity() {
+    return Integer.unity
+  }
   get _inv() {
     return new Adele(this._unity, this, this._zero)
   }
   _mul(a) {
-    return (
-      this.isZero || a.isZero
-      ? this._zero
-      : this.isUnity
-      ? a
-      : a.isUnity
-      ? this
-      : (
-        ((_, a) => (
-          ((arr) => (
-            Array(_.length).fill().map((_, i) => i).forEach((i) => (
-              Array(a.length).fill().map((_, j) => j).forEach((j) => (
-                (([q, r]) => (
-                  arr[i + j] = r,
-                  i + j + 1 < arr.length && (arr[i + j + 1] += q)
-                ))
-                ((arr[i + j] + _[i] * a[j]).divmod(0x100))
-              ))
-            )),
-            new Integer(arr)
-          ))
-          (
-            new Uint8Array(_.length + a.length - 1)
-          )
-        ))
-        (this._, a._)
-      )
-    )
+    if (this.isZero || a.isZero) {
+      return this._zero
+    } else
+    if (this.isUnity) {
+      return a
+    } else
+    if (a.isUnity) {
+      return this
+    } else
+    {
+      let _ = this._; a = a._
+      let _len = _.length, alen = a.length, ualen = _len + alen
+      let ua = new Uint8Array(_len + alen)
+      let i, j, q, r
+
+      for (i = 0; i < _len; i ++) {
+        for (j = 0; j < alen; j ++) {
+          [q, r] = (ua[i + j] + _[i] * a[j]).divmod(0x100)
+          ua[i + j] = r
+          i + j + 1 < ualen && (ua[i + j + 1] += q)
+        }
+      }
+
+      return new Integer(ua)
+    }
   }
 }
 Integer.zero = new Integer
@@ -563,56 +562,66 @@ class Arch extends Algebraic {
       (ord || 0, arg || 0)
     )
   }
-  get finalize() { return this }
+  get finalize() {
+    return this
+  }
   coerce(a) {
     return (
-      this.eq(a)
-      ? [this, a]
-      : a === 0
-      ? [this, Arch.zero]
-      : (
-        a > 0
-        ? [this, new Arch(a.log, 0)]
-        : [this, new Arch((-a).log, 0.5)]
-      )
+      this.eq(a) ? [this, a] :
+      a === 0 ? [this, Arch.zero] :
+      a > 0 ? [this, new Arch(a.log)] :
+      [this, new Arch((- a).log, 0.5)]
     )
   }
   _eql(a) {
     return (
-      this.ord === a.ord && this.arg === a.arg
+      this.ord === a.ord &&
+      this.arg === a.arg
     )
   }
-  get _zero() { return Arch.zero }
+  get _zero() {
+    return Arch.zero
+  }
   get _neg() {
     return new Arch(this.ord, this.arg + 0.5)
   }
   _add(a) {
     return (
-      a.isZero
-      ? this
-      : this._eql(a._neg)
-      ? this.zero
-      : this.ord < a.ord
-      ? a._add(this)
-      : this._mul(this._inv._mul(a).succ)
+      a.isZero ? this :
+      this._eql(a._neg) ? this.zero :
+      this.ord < a.ord ? a._add(this) :
+      this._mul(
+        this._inv._mul(a).succ
+      )
     )
   }
-  get _unity() { return Arch.unity }
+  get _unity() {
+    return Arch.unity
+  }
   get _inv() {
-    return new Arch(-this.ord, -this.arg)
+    return new Arch(
+      - this.ord,
+      - this.arg
+    )
   }
   _mul(a) {
     return (
       a.isZero
       ? a
-      : new Arch(this.ord + a.ord, this.arg + a.arg)
+      : new Arch(
+        this.ord + a.ord,
+        this.arg + a.arg
+      )
     )
   }
   get shift() {
     return (
       this.isZero
       ? this
-      : new Arch(this.ord + 1, this.arg)
+      : new Arch(
+        this.ord + 1,
+        this.arg
+      )
     )
   }
   get succ() {
@@ -620,9 +629,7 @@ class Arch extends Algebraic {
   }
   get exp() {
     return (
-      this.isZero
-      ? Arch.unity
-      : (
+      this.isZero ? Arch.unity : (
         ((ord, arg) => (
           arg < 0.5 || (arg -= 1),
           (arg *= PI2),
@@ -637,11 +644,8 @@ class Arch extends Algebraic {
   }
   get log() {
     return (
-      this.isZero
-      ? undefined
-      : this.isUnity
-      ? Arch.zero
-      : (
+      this.isZero ? undefined :
+      this.isUnity ? Arch.zero : (
         ((ord, arg) => (
           arg < 0.5 || (arg -= 1),
           (arg *= PI2),
@@ -659,4 +663,6 @@ Arch.zero = new Arch
 Arch.unity = new Arch(0, 0)
 Arch.cast = (a) => Arch.zero.cast(a)
 
-module.exports = { Algebraic, toUint8Array, Integer, Adele, PI2, Arch }
+typeof module === 'undefined' || (
+  module.exports = { Algebraic, toUint8Array, Integer, Adele, PI2, Arch }
+)
